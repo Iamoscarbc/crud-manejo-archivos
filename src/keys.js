6a7c53ac563ea07469a263795f6b885d54bed7f2
Iamoscarbc <Iamoscarbc@users.noreply.github.com>
2019-08-21 01:06:08 -0500

    Configuración de base de datos agregada

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'database_products'
+        host: 'us-cdbr-iron-east-02.cleardb.net',
+        user: 'b2f41418fee361',
+        password: 'a75c352d',
+        database: 'heroku_c4a6d41e035fa03'
     }
-
+    //mysql://b2f41418fee361:a75c352d@us-cdbr-iron-east-02.cleardb.net/heroku_c4a6d41e035fa03?reconnect=true
 };
