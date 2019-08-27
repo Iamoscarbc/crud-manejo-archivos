e60ab6338cea5010106a4adf3f83492df5a9cc32
Iamoscarbc <Iamoscarbc@users.noreply.github.com>
2019-08-26 20:40:07 -0500

    eliminando imagenes y entregando carpeta uploads

--- src/routes/uploads.js
+++ src/routes/uploads.js
@@ -6,6 +6,10 @@ const path = require('path');
 const rutaUpload = path.join(__dirname, '../public/uploads');
 
 //Routes
+router.get('/', (req,res) => {
+    res.sendFile(path.join(rutaUpload));
+})
+
 router.get('/:img', (req,res) => {
     res.sendFile(path.join(rutaUpload, req.params.img ))
 })
