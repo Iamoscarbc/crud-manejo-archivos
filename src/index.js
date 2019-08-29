7841f61a51e13d7e5e9a3cf7da9f95156a7d8dad
Iamoscarbc <Iamoscarbc@users.noreply.github.com>
2019-08-28 22:11:33 -0500

    headers

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ app.use((req, res, next ) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,  DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
